22bf5396809cff454cec19036c3a6996324e5cfb
tritlo <mpg@mpg.is>
2014-03-28 11:56:02 +0000

    some size and color twiddle

--- Ball.js
+++ Ball.js
@@ -1,14 +1,15 @@
 function Ball(descr){
     var modelFile = (lowdef) ? "sphere.ply" : "newsphere.ply";
     this.__proto__.setup(modelFile);
-    descr.shininess = 1000;
+    descr.shininess = 200;
     this.setup(descr);
     //this.rotate(Math.PI,[0,1,0]);
     this.origHeight = this.loc[1];
     this.currentTrans = [0,0,0,0];
-    this.setColor(descr.color || [0.85,0.64,0.13,1.0]);
+    //this.setColor(descr.color || [0.85,0.64,0.13,1.0]);
+    this.setColor(descr.color || [1.0,0.0,0.0,1.0]);
     //this.scale([0.8,0.8,0.8]);
-    this.scale([0.4,0.4,0.4]);
+    this.scale([0.48,0.48,0.48]);
     this.type = "Ball";
 };
 
